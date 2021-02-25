08ee599c4d99132e67f314613b03602db09266e0
DawidKwiatkowski740 <dawidkwiatkowski06@gmail.com>
2021-02-25 18:24:30 +0100

    zad1

---
+++ zadania/src/zad1.java
@@ -0,0 +1,37 @@
+public class zad1 {
+    public static void main(String[] args) {
+
+
+        int myIntA = 1;
+        int myIntB = 234;
+        int myIntC = 34;
+        double myDoubleA = 2.75;
+        double myDoubleB = 2.0;
+        double myDoubleC = 0.25;
+        char myCharA = 'D';
+        char myCharB = 'D';
+        char myCharC = 'K';
+        String myStringA = "Dawid";
+        String myStringB = "Kwiatkowski";
+
+        System.out.println(myIntA);
+        System.out.println(myIntB);
+        System.out.println(myIntC);
+
+        System.out.println(myDoubleA);
+        System.out.println(myDoubleB);
+        System.out.println(myDoubleC);
+
+        System.out.println("Moje inicjały to: "+ myCharA + myCharB + myCharC);
+
+        String sumString = myStringA + myStringB;
+        System.out.println(sumString + "\nChyba.");
+
+
+    }
+
+
+
+
+
+}
