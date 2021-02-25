f9962e8c3c594cbea12041b245890a0d60e064f6
DawidKwiatkowski740 <dawidkwiatkowski06@gmail.com>
2021-02-25 18:39:55 +0100

    zad2

---
+++ zadania/src/zad2.java
@@ -0,0 +1,19 @@
+public class zad2 {
+    public static void main(String[] args){
+
+        int myIntA = 2;
+        int myIntB = 4;
+
+        int sumA = myIntA * myIntA;
+        int sumB = myIntB * myIntB;
+        int sum = sumA + sumB;
+
+        System.out.println(sum);
+
+
+
+
+
+    }
+
+}
