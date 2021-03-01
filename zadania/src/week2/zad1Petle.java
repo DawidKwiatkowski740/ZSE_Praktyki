58047c63634c8492065abd6d7bebc50f619ed2da
DawidKwiatkowski740 <dawidkwiatkowski06@gmail.com>
2021-03-01 18:18:59 +0100

    zadanie week two pętle

---
+++ zadania/src/week2/zad1Petle.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class zad1Petle {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 21; i=i+2) {
+            System.out.println(i + "\t");
+
+        }
+
+
+    }
+}
---
+++ zadania/src/week2/zad2Petle.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class zad2Petle {
+
+    public static void main(String[] args) {
+        for (int i = 30; i >= 0; i = i - 3) {
+            System.out.println(i + "\t");
+
+
+        }
+
+    }
+}
